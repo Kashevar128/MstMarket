9f14790c69e4af767bfb70cd1335c38fc199e503
mstislav <steve128@yandex.ru>
2023-03-13 14:06:40 +0300

    Введение новых продуктов и категорий

--- flyway/V1_0__products.sql
+++ flyway/V1_0__products.sql
@@ -19,20 +19,27 @@ create table products
 
 insert into categories (title) values ('Еда');
 insert into categories (title) values ('Хозтовары');
+insert into categories (title) values ('Напитки');
 
 insert into products (title, price, category_id, visible)
-values ('Молоко', 100.20, 1, true),
-       ('Хлеб', 80.20, 1, true),
-       ('Сыр', 90.20, 1, true),
+values ('Молоко', 100.00, 1, true),
+       ('Хлеб', 80.00, 1, true),
+       ('Сыр', 90.00, 1, true),
        ('Масло', 320.00, 1, true),
        ('Бублики', 50.00, 1, true),
        ('Пирожок', 30.00, 1, true),
        ('Огурцы', 140.00, 1, true),
        ('Помидоры', 160.00, 1, true),
-       ('Лимонад', 100.00, 1, true),
+       ('Лимонад', 100.00, 3, true),
        ('Подсолнечное масло', 110.00, 1, true),
        ('Мороженное', 70.00, 1, true),
-       ('Coca-Cola 0.9 л.', 87.00, 1, true);
+       ('Coca-Cola', 87.00, 3, true),
+       ('Sprite', 89.00, 3, true),
+       ('Моющее средство', 50.00, 2, true),
+       ('Губки для мытья посуды', 20.00, 2, true),
+       ('Мыло', 10.00, 2, true),
+       ('Хоз. перчатки', 15.00, 2, true),
+       ('Cтиральный порошок', 40.00, 2, true);
 
 create table orders
 (
